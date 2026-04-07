65e1368d77665870f01f249b7ca49a575c808783
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-07 17:20:45 -0300

    feat(test-factories): updating make-question   The make-question has been improved to be able to generate unique content using the faker library.

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,13 +1,18 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import { Question, type QuestionProps } from '@/domain/forum/enterprise/entities/question'
 
-export function makeQuestion(overrride: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityId(),
-    title: 'Example',
-    content: 'example-content',
-    ...overrride,
-  })
+export function makeQuestion(overrride: Partial<QuestionProps> = {}, id?: UniqueEntityId) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...overrride,
+    },
+    id,
+  )
 
   return question
 }
