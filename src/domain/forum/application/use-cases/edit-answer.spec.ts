bed05bdcd484be85bc5d46e1b29c1a2f738a3ad5
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-08 15:14:20 -0300

    feat(forum-use-case): create edit-answer   The edit-answer use case was created. Changes were made to the main answer repository in memory. A test for it was created and validated.

--- src/domain/forum/application/use-cases/edit-answer.spec.ts
+++ src/domain/forum/application/use-cases/edit-answer.spec.ts
@@ -4,18 +4,18 @@ import { beforeEach, describe, expect, it } from 'vitest'
 
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
-import { DeleteAnswersUseCase } from './delete-answer'
+import { EditAnswerUseCase } from './edit-answer'
 
 let inMemoryAnswersRepository: InMemoryAnswersRepository
-let sut: DeleteAnswersUseCase
+let sut: EditAnswerUseCase
 
-describe('Delete Answer', () => {
+describe('Edit Answer', () => {
   beforeEach(() => {
     inMemoryAnswersRepository = new InMemoryAnswersRepository()
-    sut = new DeleteAnswersUseCase(inMemoryAnswersRepository)
+    sut = new EditAnswerUseCase(inMemoryAnswersRepository)
   })
 
-  it('should be able to delete a answer', async () => {
+  it('should be able to edit a answer', async () => {
     const newAnswer = makeAnswer({
       authorId: new UniqueEntityId('author-1'),
     })
@@ -25,12 +25,15 @@ describe('Delete Answer', () => {
     await sut.execute({
       answerId: newAnswer.id.toString(),
       authorId: 'author-1',
+      content: 'Answer updated content',
     })
 
-    expect(inMemoryAnswersRepository.items).toHaveLength(0)
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: 'Answer updated content',
+    })
   })
 
-  it('should be able to delete a answer from another user', async () => {
+  it('should be able to edit a answer from another user', async () => {
     const newAnswer = makeAnswer({
       authorId: new UniqueEntityId('author-1'),
     })
@@ -41,9 +44,12 @@ describe('Delete Answer', () => {
       sut.execute({
         answerId: newAnswer.id.toString(),
         authorId: 'author-2',
+        content: 'Answer updated content',
       }),
     ).rejects.toBeInstanceOf(Error)
 
-    expect(inMemoryAnswersRepository.items).toHaveLength(1)
+    expect(inMemoryAnswersRepository.items[0]).toMatchObject({
+      content: newAnswer.content,
+    })
   })
 })
