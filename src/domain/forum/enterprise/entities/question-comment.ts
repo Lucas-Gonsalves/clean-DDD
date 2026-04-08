ee3e7fcaf58a37c18de15be833fbc34aa1410da3
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-08 17:29:31 -0300

    feat(enterprise-entities): create comment   With the creation of the new comment entity, `comment`, the `question-comment` and `answer-comment` entities were refactored using the `comment` class as a framework for their creation.

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,39 +1,15 @@
-import { Entity } from '@/core/entities/entity'
 import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import type { Optional } from '@/core/types/optional'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityId
-  answerId: UniqueEntityId
-  content: string
-  createdAt: Date
-  updatedAt?: Date | null
+import { Comment, type CommentProps } from './comment'
+
+export interface QuestionCommentProps extends CommentProps {
+  questionId: UniqueEntityId
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+export class QuestionComment extends Comment<QuestionCommentProps> {
+  get questionId() {
+    return this.props.questionId
   }
 
   static create(props: Optional<QuestionCommentProps, 'createdAt'>, id?: UniqueEntityId) {
@@ -44,7 +20,6 @@ export class QuestionComment extends Entity<QuestionCommentProps> {
       },
       id,
     )
-
     return questionComment
   }
 }
