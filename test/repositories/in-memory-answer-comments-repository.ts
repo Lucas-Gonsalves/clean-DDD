a6c4a8558ee33be454eef85905f42db544b4f5a1
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-09 16:25:22 -0300

    feat(forum-use-case): create comment-on-answer   A use case for comments in forum answers was created. To enable testing, it was necessary to create a main repository and an in-memory repository.

---
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -0,0 +1,10 @@
+import type { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments'
+import type { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+
+export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepository {
+  public items: AnswerComment[] = []
+
+  async create(answerComments: AnswerComment) {
+    this.items.push(answerComments)
+  }
+}
