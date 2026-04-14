9dd84af654e3fcba905e628fbe4bf267f931f862
Lucas Gonçalves <lucasluz1710@gmail.com>
2026-04-14 15:00:01 -0300

    feat(forum-events): create answer-created-events   A new event was created on forum. All time when a new answer is created an event is created too.

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,7 +1,8 @@
-import { Entity } from '@/core/entities/entity'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
 import type { UniqueEntityId } from '@/core/entities/unique-entity-id'
 import type { Optional } from '@/core/types/optional'
 
+import { AnswerCreatedEvent } from '../events/answer-created-events'
 import { AnswerAttachmentList } from './answer-attachment-list'
 
 export interface AnswerProps {
@@ -13,7 +14,7 @@ export interface AnswerProps {
   updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
   get authorId() {
     return this.props.authorId
   }
@@ -65,6 +66,13 @@ export class Answer extends Entity<AnswerProps> {
       },
       id,
     )
+
+    const isNewAnswer = !id
+
+    if (isNewAnswer) {
+      answer.addDomainEvent(new AnswerCreatedEvent(answer))
+    }
+
     return answer
   }
 }
